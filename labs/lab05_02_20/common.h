68590aaa18843ea0a7dab7d7c4f370138b6fab46
liz <liz@augsburg.edu>
2020-02-19 16:21:14 -0600

    added from the previous lab

---
+++ labs/lab05_02_20/common.h
@@ -0,0 +1,44 @@
+// Header guard
+// Prevent from cicular references
+#ifndef COMMON_H_
+#define COMMON_H_
+
+// Frequency of CPU. For reusing code across different processors with different clock speed
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+// Essential libraries
+#include <avr/io.h>
+#include <inttypes.h>
+
+// Definitions of each pin
+#define PIN0 0
+#define PIN1 1
+#define PIN2 2
+#define PIN3 3
+#define PIN4 4
+#define PIN5 5
+#define PIN6 6
+#define PIN7 7
+
+// Set a bit to 1
+#define SET_BIT(reg, pin) (reg |= (1 << pin))
+// Set a bit to 0
+#define CLEAR_BIT(reg, pin) (reg &= ~(1 << pin))
+// Toggle the value of the bit
+#define TOGGLE_BIT(reg, pin) (reg ^= (1 << pin))
+
+// Configure as a port in a DDR register as output
+#define CONFIG_OUTPUT(port, pin) (SET_BIT(port, pin))
+// Configure as a port in a DDR register as input
+#define CONFIG_INPUT(port, pin) (CLEAR_BIT(port, pin))
+
+typedef struct{
+  volatile uint8_t * ddr;
+  volatile uint8_t * port;
+  uint8_t pin;
+  volatile uint8_t * portin;
+} IO_struct;
+
+#endif
